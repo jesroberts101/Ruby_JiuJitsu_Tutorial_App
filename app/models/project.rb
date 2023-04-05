7a6a22a58e836075b85281fe34361137381662ce
root <jesroberts101@gmail.com>
2023-04-05 19:15:12 +0000

    Added Tests from GE06 - up to #3.4

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,6 @@
 class Project < ApplicationRecord
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :rating, presence: true
+    validates :published, presence: true
 end
