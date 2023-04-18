4d5f8f7f42e9d2c29f47a2439f0ad6c4284d67ae
root <jesroberts101@gmail.com>
2023-04-18 18:03:31 +0000

    Added image file to show and new and edit (doesnt work for index)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -65,6 +65,6 @@ class ProjectsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def project_params
-      params.require(:project).permit(:title, :description, :rating, :published)
+      params.require(:project).permit(:title, :description, :rating, :published, :image)
     end
 end
