fb790290177c22d8a9279a6a1975cd34522e8562
root <jesroberts101@gmail.com>
2023-04-26 20:36:47 +0000

    bootstrap not working for signup and log in

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,8 +1,15 @@
+require 'rails_helper'
+
 RSpec.feature "HomePages", type: :feature do
-    scenario "The visitor should see projects" do
-      visit root_path
-      expect(page).to have_text("Projects")
-    end
+  scenario "The visitor should see projects" do
+    visit root_path
+    expect(page).to have_text("Projects")
   end
   
-  
+  describe 'GET /' do
+    it 'displays projects' do
+      visit '/'
+      expect(page).to have_content('Projects')
+    end
+  end
+end
